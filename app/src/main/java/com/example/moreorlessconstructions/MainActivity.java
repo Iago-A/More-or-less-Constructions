727bd56ce1e2846ee63dc89c4918ca54c1372b26
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2025-09-04 13:27:17 +0200

    Main activity

--- app/src/main/java/com/example/moreorlessconstructions/MainActivity.java
+++ app/src/main/java/com/example/moreorlessconstructions/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.moreorlessconstructions;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Obtain views references
+        TextView titleTextView = findViewById(R.id.title_text_view);
+        Button startButton = findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
         });
     }
 }
